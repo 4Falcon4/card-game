f486e1c33fbbc665f817bff29896b7b278b68002
4falcon4 <falcon@4falcon4.com>
2025-10-24 12:05:55 -0700

    Refactor card color styling: implement StyleBoxFlat for improved visual consistency and add border properties

--- card_layouts/back_balatro_style.gd
+++ card_layouts/back_balatro_style.gd
@@ -11,14 +11,33 @@ func _update_display() -> void:
 	texture_rect.texture = res.bottom_texture
 
 func set_color():
-	match res.deck_color:
-		res.deck_colors.NONE:
-			card_color.self_modulate = Color.TRANSPARENT
-		res.deck_colors.LIGHT:
-			card_color.self_modulate = Color.BURLYWOOD
-		res.deck_colors.DARK:
-			card_color.self_modulate = Color.DIM_GRAY
-		res.deck_colors.WHITE:
-			card_color.self_modulate = Color.WHITE
-		res.deck_colors.BLACK:
-			card_color.self_modulate = Color.BLACK
+		var style := StyleBoxFlat.new()
+		var border_w := 4
+		match res.deck_color:
+			res.deck_colors.NONE:
+				style.bg_color = Color.TRANSPARENT
+			res.deck_colors.LIGHT:
+				style.bg_color = Color.BURLYWOOD
+				style.border_color = Color.DARK_SLATE_GRAY
+			res.deck_colors.DARK:
+				style.bg_color = Color.DARK_SLATE_GRAY
+				style.border_color = Color.BURLYWOOD
+			res.deck_colors.WHITE:
+				style.bg_color = Color.WHITE
+				style.border_color = Color.DARK_SLATE_GRAY
+			res.deck_colors.BLACK:
+				style.bg_color = Color.BLACK
+				style.border_color = Color.BURLYWOOD
+	
+		style.border_width_top = border_w
+		style.border_width_bottom = border_w
+		style.border_width_left = border_w
+		style.border_width_right = border_w
+	
+		style.corner_radius_top_left = 8
+		style.corner_radius_top_right = 8
+		style.corner_radius_bottom_left = 8
+		style.corner_radius_bottom_right = 8
+	
+		card_color.add_theme_stylebox_override("panel", style)
+		card_color.self_modulate = Color(1, 1, 1, 1)
--- card_layouts/balatro_style.gd
+++ card_layouts/balatro_style.gd
@@ -14,13 +14,31 @@ func _update_display() -> void:
 	set_value()
 
 func set_color():
+	var style := StyleBoxFlat.new()
+	var border_w := 4
 	match res.current_modiffier:
 		res.modiffier.NONE:
-			card_color.self_modulate = Color.BISQUE
+			style.bg_color = Color.WHITE
+			style.border_color = Color.BLACK
 		res.modiffier.GOLD:
 			card_color.self_modulate = Color.GOLD
+			style.border_color = Color.BLACK
 		res.modiffier.STEEL:
 			card_color.self_modulate = Color.LIGHT_STEEL_BLUE
+			style.border_color = Color.BLACK
+
+	style.border_width_top = border_w
+	style.border_width_bottom = border_w
+	style.border_width_left = border_w
+	style.border_width_right = border_w
+
+	style.corner_radius_top_left = 8
+	style.corner_radius_top_right = 8
+	style.corner_radius_bottom_left = 8
+	style.corner_radius_bottom_right = 8
+
+	card_color.add_theme_stylebox_override("panel", style)
+	card_color.self_modulate = Color(1, 1, 1, 1)
 
 func set_value():
 	var text: String = ""
