732892192153210c1fbe5b5ccc4d0afc23b9fcce
4falcon4 <falcon@4falcon4.com>
2025-10-24 11:08:10 -0700

    Fix scene paths to use lowercase 'menu' for consistency across project files

--- menu/scenes/end_credits/end_credits.gd
+++ menu/scenes/end_credits/end_credits.gd
@@ -1,5 +1,5 @@
 @tool
-extends "res://Menu/scenes/credits/scrolling_credits.gd"
+extends "res://menu/scenes/credits/scrolling_credits.gd"
 
 ## Defines the path to the main menu. Hides the Main Menu button if not set.
 ## Will attempt to read from AppConfig if left empty.
