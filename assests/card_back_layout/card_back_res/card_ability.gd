ef9eaed540e69232dcb775621b9e03305f42b881
4falcon4 <falcon@4falcon4.com>
2025-10-28 11:03:25 -0700

    Add card ability resource and update card back resource properties

---
+++ assests/card_back_layout/card_back_res/card_ability.gd
@@ -0,0 +1,4 @@
+@abstract
+class_name CardAbility extends Script
+
+@abstract func preform()
--- assests/card_back_layout/card_back_res/card_back_resource.gd
+++ assests/card_back_layout/card_back_res/card_back_resource.gd
@@ -1,5 +1,7 @@
 @abstract @icon("uid://cvwcyhqx6fvdk")
 class_name CardBackResource extends Resource
 
-@export var Description: String
+@export var display_name: String
+@export var ability: CardAbility
+@export var description: String
 @export var texture: Texture2D
