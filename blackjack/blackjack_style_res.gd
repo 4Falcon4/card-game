a19f9898d508b0ad38980b743f6d6bef00439cf0
4falcon4 <falcon@4falcon4.com>
2025-11-04 14:28:35 -0800

    Change ability type from Script to CardAbility for improved functionality

--- blackjack/blackjack_style_res.gd
+++ blackjack/blackjack_style_res.gd
@@ -8,7 +8,7 @@ class_name BlackjackStyleRes extends CardResource
 @export var card_suit: suit = suit.ALL
 @export var value: int = 1
 @export var display_name: String
-@export var ability: Script
+@export var ability: CardAbility
 @export var descriptionP: String
 @export var descriptionN: String
 @export var rarrity: Rarrity = Rarrity.NONE
